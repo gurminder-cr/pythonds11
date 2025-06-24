0ece3012f77d5db954f00a8854892744734bf3bf
gurmindero7 <123065166+gurmindero7@users.noreply.github.com>
2025-06-24 12:17:56 +0530

    recursion,maths,datetime

---
+++ maths.py
@@ -0,0 +1,22 @@
+import math
+# import random 
+# import check
+
+# print(random.randint(2,40))
+print(math.factorial(7))
+print(math.sqrt(144))
+# print(math.pow)
+
+import datetime
+import time 
+print(datetime.datetime.now())
+print(datetime.date.today())
+time.sleep(5)
+
+print("Hello after 5 seconds")
+# while True:
+#     print(datetime.datetime.now())
+#     time.sleep(5)
+    
+    
+import regex #- regular expressions 
